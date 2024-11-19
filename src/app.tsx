f56c1cf6cdb2a853f6c2b1a05c7bc6c6ac7527ea
MarcRGM <marcrodniemanalo@gmail.com>
2024-11-19 19:49:43 +0800

    Added tailwind + searchBar

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import './App.css'
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-  
+  return <SearchBar />
 }
 
 export default App
---
+++ src/components/searchBar/searchBar.tsx
@@ -0,0 +1,7 @@
+import './SearchBar.css';
+
+function SearchBar() {
+    return <input id="searchBar" type="text" />
+}
+
+export default SearchBar;
