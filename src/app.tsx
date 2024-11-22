e36f739a19af89a591e807a0bc1d45455e889f4b
MarcRGM <marcrodniemanalo@gmail.com>
2024-11-22 20:15:19 +0800

    Setup Routes for Login and Register

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,20 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import routes from "./routes"; 
 
-function App() {
-  return
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                {routes.map((route, index) => (
+                    <Route
+                        key={index}
+                        path={route.path}        // The URL path for the route
+                        element={<route.component />} // The component to display 
+                    />
+                ))}
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "./components/LoginForm/LoginForm";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+
+const routes = [
+    { path: "/login", component: LoginForm },
+    { path: "/register", component: RegisterForm },
+];
+
+export default routes;
