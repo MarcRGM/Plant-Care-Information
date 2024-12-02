2bdc3e2eb99205f5a46d505542ea3b8f7aec12eb
MarcRGM <marcrodniemanalo@gmail.com>
2024-12-03 05:48:53 +0800

    Update home page layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,12 +7,19 @@ const Home = () => {
 
   return (
     <div className="flex justify-center items-center w-full h-screen px-4 sm:px-6 md:px-8">
-      <div className="w-full max-h-96 max-w-xl sm:max-w-2xl md:max-w-3xl">
-      <img 
-          src="../../public/images/logo.png" 
-          alt="Logo" 
-          className="w-32 h-auto mb-4 mx-auto" // Adjust width and margin
-        />
+      <div className="w-full h-screen flex flex-col max-h-96 max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-3xl">
+        <div className="flex items-center">
+          <img 
+              src="../../public/images/logo.png" 
+              alt="Logo" 
+              className="w-32 px-2 sm:px-2 md:px-4 lg:px-6 sm:w-48 md:w-56 lg:w-64 h-auto mb-4" 
+          />
+          <div className="">
+            <p className="font-medium text-lg sm:text-xl md:text-2xl lg:text-3xl text-[#425F57]">Welcome to</p>
+            <span className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-[#425F57]">Water-Down</span>
+            <p className="font-medium text-lg sm:text-xl md:text-2xl lg:text-3xl text-[#425F57]">Plant-care Information Website</p>
+          </div>
+        </div>
         <SearchBar onSearch={handleSearch} />
       </div>
     </div>
